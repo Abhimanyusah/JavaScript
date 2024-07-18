9d50bf4b6b4933b3ffb0b0239ccda8dd6c6b72a4
Abhimanyu Sah <49836009+Abhimanyusah@users.noreply.github.com>
2024-07-18 06:17:29 +0000

    Add

---
+++ Test.js
@@ -0,0 +1,11 @@
+let score = "100abc"
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+let valueInNumber=Number(score)
+console.log(valueInNumber);
+
+let isLoggedIn=0
+let booleanIsLoggedIn=Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
